eae56cf0d818750a8df5200f05caeb3c9629d08f
Eziamaka Promise <107619759+eziamakapromise@users.noreply.github.com>
2023-03-16 05:51:27 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const see = document.getElementById("seebtn");
+const more = document.getElementById("seemore");
+const date = document.getElementById("date");
+const show = document.getElementById("showx");
+const ads = document.getElementById("ads");
+
+see.addEventListener("click", function () {
+ if (more.style.display = "block") {
+  more.style.display = "block"
+ }
+ else {
+  more.style.display = "none"
+ }
+})
+show.addEventListener("click", function () {
+ ads.style.display = "none"
+})
